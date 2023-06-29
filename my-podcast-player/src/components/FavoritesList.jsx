21fc88c963cf4f6067dd9b48e8be62d967e0f66e
daddybear <sihlebarrett@gmail.com>
2023-06-29 11:16:46 +0200

    favs

--- my-podcast-player/src/components/FavoritesList.jsx
+++ my-podcast-player/src/components/FavoritesList.jsx
@@ -1,9 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { formatDate } from "../helpers/formatDate";
 
+//
+
+//
+
 const FavoritesList = ({ favorites, setFavorites }) => {
+  const [sortOrder, setSortOrder] = useState("none"); //
   useEffect(() => {}, [favorites]);
+  //
+  const sortedEpisodes = (episodes) => {
+    if (sortOrder === "titleAZ") {
+      return [...episodes].sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortOrder === "titleZA") {
+      return [...episodes].sort((a, b) => b.title.localeCompare(a.title));
+    } else if (sortOrder === "recent") {
+      return [...episodes].sort(
+        (a, b) => new Date(b.addedAt) - new Date(a.addedAt)
+      );
+    } else if (sortOrder === "oldest") {
+      return [...episodes].sort(
+        (a, b) => new Date(a.addedAt) - new Date(b.addedAt)
+      );
+    } else {
+      return episodes;
+    }
+  };
+
+  const handleSortChange = (event) => {
+    setSortOrder(event.target.value);
+  };
+  //
+
   const groupedFavorites = {};
 
   favorites.forEach((favorite) => {
@@ -26,16 +55,21 @@ const FavoritesList = ({ favorites, setFavorites }) => {
   });
 
   const removeFromFavorites = (episode) => {
-    const updatedFavorites = favorites.filter(
-      (fav) => fav.episode.id !== episode.id
-    );
+    const updatedFavorites = favorites.filter((fav) => fav.id !== episode.id);
     setFavorites(updatedFavorites);
     localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
   };
-
   return (
     <div>
       <h2>Favorites</h2>
+      <select onChange={handleSortChange}>
+        <option value="none">Sort by...</option>
+        <option value="titleAZ">Title (A-Z)</option>
+        <option value="titleZA">Title (Z-A)</option>
+        <option value="recent">Most recent</option>
+        <option value="oldest">Oldest</option>
+      </select>
+      ;
       {Object.entries(groupedFavorites).map(([showId, show]) => (
         <div key={showId}>
           <h3>
@@ -49,22 +83,27 @@ const FavoritesList = ({ favorites, setFavorites }) => {
                 </Link>
               </h4>
               <ul>
-                {season.episodes.map((episode) => (
-                  <li key={episode.id}>
-                    <h3>{episode.title}</h3>
-                    <p>
-                      <strong>Episode:</strong> {episode.episode}
-                    </p>
-                    <p>{episode.description}</p>
-                    <audio controls>
-                      <source src={episode.file} type="audio/mpeg" />
-                      Your browser does not support the audio element.
-                    </audio>
-                    <button onClick={() => removeFromFavorites(episode)}>
-                      Remove from favorites
-                    </button>
-                  </li>
-                ))}
+                {sortedEpisodes(season.episodes).map(
+                  (
+                    episode //
+                  ) => (
+                    <li key={episode.id}>
+                      <h3>{episode.title}</h3>
+                      <p>
+                        <strong>Episode:</strong> {episode.episode}
+                      </p>
+                      <p>{episode.description}</p>
+                      <p>Added on {formatDate(new Date(episode.addedAt))}</p> //
+                      <audio controls>
+                        <source src={episode.file} type="audio/mpeg" />
+                        Your browser does not support the audio element.
+                      </audio>
+                      <button onClick={() => removeFromFavorites(episode)}>
+                        Remove from favorites
+                      </button>
+                    </li>
+                  )
+                )}
               </ul>
             </div>
           ))}
--- my-podcast-player/src/helpers/formatDate.jsx
+++ my-podcast-player/src/helpers/formatDate.jsx
@@ -3,5 +3,7 @@ export const formatDate = (date) => {
     month: "short",
     day: "numeric",
     year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
   }).format(date);
 };
